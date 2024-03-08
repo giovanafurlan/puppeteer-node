80d3da3e3a8fe9505308c2d7d8646bcc43e949ff
giovanaFurlan <giovananelofurlan@gmail.com>
2024-03-08 15:58:56 -0300

    fix: Funcionando local

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const port = 3001;
 app.get("/api", async (req, res) => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-      "--disable-features=site-per-process",
-    ],
+    // args: [
+    //   "--disable-setuid-sandbox",
+    //   "--no-sandbox",
+    //   "--single-process",
+    //   "--no-zygote",
+    //   "--disable-features=site-per-process",
+    // ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
@@ -32,90 +32,98 @@ app.get("/api", async (req, res) => {
     await page.setViewport({ width: 1280, height: 800 });
     await page.goto(parametro, { waitUntil: "networkidle2", timeout: 60000 }); // aumentando para 60 segundos (60000 milissegundos)
 
-    await page.click("a.main__sign-in-link");
-
-    // preencher o campo de e-mail
-    await page.type(
-      'input[name="session_key"]',
-      "backupgiovanafurlan@outlook.com"
-    );
-
-    // preencher o campo de senha
-    await page.type('input[name="session_password"]', "Fur0412*");
-
-    // clicar no botão de login
-    await page.click('button[aria-label="Sign in"]');
-
-    // aguardar um pouco para permitir que a página seja carregada completamente após o login
-    await page.waitForTimeout(5000); // Ajuste o tempo conforme necessário
-
-    // redirecionar para a página do perfil
-    await page.goto("https://www.linkedin.com/in/giovana-furlan/");
+    let sobre;
+    let funcao;
+    let localizacao;
+    let experiencias;
 
     // pegar título
-    let title = await page.title();
+    const title = await page.title();
 
-    // pegar conteúdo sobre
-    const sobre = await page.evaluate(() => {
-      const span = document.querySelector(".core-section-container__content");
-      if (span) {
-        return span.textContent.trim();
-      }
-      return null;
-    });
+    if (title == "Sign Up | LinkedIn") {
+      // clica no link de login
+      await page.click("a.main__sign-in-link");
 
-    // pegar conteúdo função
-    const funcao = await page.evaluate(() => {
-      const span = document.querySelector(".top-card-layout__headline");
-      if (span) {
-        return span.textContent.trim();
-      }
-      return null;
-    });
-
-    // pegar conteúdo localização
-    const localizacao = await page.evaluate(() => {
-      const span = document.querySelector(
-        "div.not-first-middot span:first-child"
+      // preencher o campo de e-mail
+      await page.type(
+        'input[name="session_key"]',
+        "backupgiovanafurlan@outlook.com"
       );
-      if (span) {
-        return span.textContent.trim();
-      }
-      return null;
-    });
 
-    // pegar conteúdo experiências
-    const experiencias = await page.evaluate(() => {
-      const experienceItems = document.querySelectorAll(
-        'section[data-section="experience"] .experience-item'
-      );
-      const experiencesArray = [];
+      // preencher o campo de senha
+      await page.type('input[name="session_password"]', "Fur0412*");
 
-      experienceItems.forEach((item) => {
-        const empresa = item
-          .querySelector(".experience-item__subtitle")
-          .textContent.trim();
-        const duracao = item.querySelector(".date-range").textContent.trim();
-        const localizacao = item
-          .querySelectorAll(".experience-item__meta-item")[1]
-          .textContent.trim();
-        const descricao = item
-          .querySelector(".show-more-less-text__text--less")
-          .textContent.trim();
+      // clicar no botão de login
+      await page.click('button[aria-label="Sign in"]');
 
-        experiencesArray.push({
-          empresa,
-          duracao,
-          localizacao,
-          descricao,
-        });
+      // aguardar um pouco para permitir que a página seja carregada completamente após o login
+      await page.waitForTimeout(5000); // Ajuste o tempo conforme necessário
+
+      // redirecionar para a página do perfil
+      await page.goto("https://www.linkedin.com/in/giovana-furlan/");
+    } else {
+      // pegar conteúdo sobre
+      sobre = await page.evaluate(() => {
+        const span = document.querySelector(".core-section-container__content");
+        if (span) {
+          return span.textContent.trim();
+        }
+        return null;
       });
 
-      return experiencesArray;
-    });
+      // pegar conteúdo função
+      funcao = await page.evaluate(() => {
+        const span = document.querySelector(".top-card-layout__headline");
+        if (span) {
+          return span.textContent.trim();
+        }
+        return null;
+      });
+
+      // pegar conteúdo localização
+      localizacao = await page.evaluate(() => {
+        const span = document.querySelector(
+          "div.not-first-middot span:first-child"
+        );
+        if (span) {
+          return span.textContent.trim();
+        }
+        return null;
+      });
+
+      // pegar conteúdo experiências
+      experiencias = await page.evaluate(() => {
+        const experienceItems = document.querySelectorAll(
+          'section[data-section="experience"] .experience-item'
+        );
+        const experiencesArray = [];
+
+        experienceItems.forEach((item) => {
+          const empresa = item
+            .querySelector(".experience-item__subtitle")
+            .textContent.trim();
+          const duracao = item.querySelector(".date-range").textContent.trim();
+          const localizacao = item
+            .querySelectorAll(".experience-item__meta-item")[1]
+            .textContent.trim();
+          const descricao = item
+            .querySelector(".show-more-less-text__text--less")
+            .textContent.trim();
+
+          experiencesArray.push({
+            empresa,
+            duracao,
+            localizacao,
+            descricao,
+          });
+        });
+
+        return experiencesArray;
+      });
+    }
 
     // Retornando a resposta como JSON
-    res.json({ title });
+    res.json({ title, sobre, funcao, localizacao, experiencias });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
@@ -127,5 +135,3 @@ app.get("/api", async (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-
